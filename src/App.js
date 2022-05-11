8324ed4159de52afb05cc644ebc5513772092d93
matheus <matheusbahia10@gmail.com>
2022-05-11 18:26:48 -0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './pages/Login/index';
 import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/header';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
