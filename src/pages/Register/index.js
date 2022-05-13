2db0a7b1e3095b12fdd723ea7e46273ea8923e76
matheus <matheusbahia10@gmail.com>
2022-05-13 09:26:25 -0300

    configurando rotas

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalContainer';
 
-export default function Login() {
+export default function Register() {
   return (
     <Container>
       <h1>Login</h1>
